48cbb77d7bdf18e8503c8c20d405c99855aae6ed
Shashank <shashank.expert@gmail.com>
2025-01-02 23:37:23 +0530

    Lec 49: Added example buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import stateImage from './assets/state-mgmt.png';
 import { CORE_CONCEPTS } from './data.js'
 import Header from './Components/Header.jsx';
 import CoreConcept from './Components/CoreConcept.jsx';
+import TabButton from './TabButton/TabButton.jsx';
 
 function App() {
   return (
@@ -17,6 +18,15 @@ function App() {
           <CoreConcept title ={CORE_CONCEPTS[2].title} img = {CORE_CONCEPTS[2].image} descrtion = {CORE_CONCEPTS[2].description}/>
           <CoreConcept title ="State" img = {stateImage} descrtion = "React-managed data which, when changed, causes the component to re-render & the UI to update."/>
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import reactImg from '../assets/react-core-concepts.png';
+import "./Header.css"
 
 const reactDescription = ['Fundamnetal', 'Crucial', 'Core', 'Significant'];
 
